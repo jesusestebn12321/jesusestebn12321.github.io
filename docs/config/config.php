5097baccb619ca3b1a89c115f082b54ce7b4d71b
Jayden Pollard <jesusesteban12321@gmail.com>
2021-03-20 23:10:53 -0400

    trial

--- docs/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-define('APP_NAME', 'Curriculum');
-define('APP_HOST', 'http://localhost/curriculum');
-define('APP_STATIC', 'http://localhost/curriculum/public');
-/*define('EMAIL', 'jesusesteban12321@gmail.com');*/
-
- ?>
